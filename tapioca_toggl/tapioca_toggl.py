5fe71ce9b863bcd4c9a2da4274e3b3ad14f72af0
Raphael Pierzina <raphael@hackebrot.de>
2015-11-28 14:34:27 +0100

    Implement TogglClientAdapter with HTTPBasicAuth

---
+++ tapioca_toggl/tapioca_toggl.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from tapioca import (
+    JSONAdapterMixin,
+    TapiocaAdapter,
+    generate_wrapper_from_adapter,
+)
+
+from requests.auth import HTTPBasicAuth
+
+from .resource_mapping import RESOURCE_MAPPING
+
+
+class TogglClientAdapter(JSONAdapterMixin, TapiocaAdapter):
+    api_root = 'https://www.toggl.com/api/v8/'
+    resource_mapping = RESOURCE_MAPPING
+
+    def get_request_kwargs(self, api_params, *args, **kwargs):
+        params = super(TogglClientAdapter, self).get_request_kwargs(
+            api_params, *args, **kwargs
+        )
+
+        access_token = api_params.get('access_token')
+
+        if access_token:
+            params['auth'] = HTTPBasicAuth(
+                api_params.get('access_token'),
+                'api_token'
+            )
+        else:
+            params['auth'] = HTTPBasicAuth(
+                api_params.get('user'),
+                api_params.get('password')
+            )
+
+        return params
+
+    def get_iterator_list(self, response_data):
+        return response_data
+
+    def get_iterator_next_request_kwargs(self, iterator_request_kwargs,
+                                         response_data, response):
+        pass
+
+
+Toggl = generate_wrapper_from_adapter(TogglClientAdapter)
