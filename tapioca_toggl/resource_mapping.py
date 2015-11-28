132256b1fc1a34dd82e04114bbaa1a5405309771
Raphael Pierzina <raphael@hackebrot.de>
2015-11-28 14:50:55 +0100

    Start RESOURCE_MAPPING with me and projects

---
+++ tapioca_toggl/resource_mapping.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+    'me': {
+        'resource': 'me',
+        'docs': 'https://github.com/toggl/toggl_api_docs/blob/master/chapters/users.md#get-current-user-data'
+    },
+    'projects': {
+        'resource': 'projects',
+        'docs': 'https://github.com/toggl/toggl_api_docs/blob/master/chapters/projects.md#create-project'
+    },
+}
